3189f431f581da3a896f8f2e717eb6915f714f94
ezglowai <ezglowai@gmail.com>
2025-07-08 18:09:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import asyncio
+from scraper import scrape_vimeo  # This assumes you already have a scraper.py file with this function defined
+
 async def main():
     await scrape_vimeo()
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
