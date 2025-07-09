bb69971990824f29668c4b2441de94b1c74ee8da
ezglowai <ezglowai@gmail.com>
2025-07-08 18:00:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from playwright.async_api import async_playwright
 
-def run():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context(user_agent="Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X)")
-        page = context.new_page()
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page()
+        await page.goto("https://vimeo.com/therealbrokerage")
+        await page.screenshot(path="example.png")
+        await browser.close()
 
-        page.goto("https://vimeo.com/therealbrokerage")
+asyncio.run(run())
 
-        # TODO: Auto-click 'Load More' until all videos load
-        # TODO: Click each video and download transcript if available
-
-        page.wait_for_timeout(5000)
-        browser.close()
-
-run()
